def5459d5ff56fac389a1fdaabe398e924dde14b
Tom B <tb1280@nyu.edu>
2016-10-05 17:46:55 -0400

    fix doc errors

--- R/plotPCA3D.R
+++ R/plotPCA3D.R
@@ -6,26 +6,25 @@
 #' a 3D exploration of the PCA.
 #'
 #' @param object a DESeqTransform object, with data in assay(x), produced for example by either rlog or varianceStabilizingTransformation.
-#' @param intergroup interesting groups: a character vector of names in colData(x) to use for grouping
+#' @param intgroup interesting groups: a character vector of names in colData(x) to use for grouping
 #' @param ntop number of top genes to use for principal components, selected by highest row variance
 #' @param returnData should the function only return the data.frame of PC1, PC2 and PC3 with intgroup covariates for custom plotting (default is FALSE)
 #' @return An object created by plot_ly, which can be assigned and further customized.
 #' @export
 plotPCA3D <- function (object, intgroup = "condition", ntop = 500, returnData = FALSE){
-  rv <- rowVars(assay(object))
-  select <- order(rv, decreasing = TRUE)[seq_len(min(ntop,
-                                                     length(rv)))]
-  pca <- prcomp(t(assay(object)[select, ]))
+  rv <- DESeq2::rowVars(DESeq2::assay(object))
+  select <- order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
+  pca <- prcomp(t(DESeq2::assay(object)[select, ]))
   percentVar <- pca$sdev^2/sum(pca$sdev^2)
-  if (!all(intgroup %in% names(colData(object)))) {
+  if (!all(intgroup %in% names(DESeq2::colData(object)))) {
     stop("the argument 'intgroup' should specify columns of colData(dds)")
   }
-  intgroup.df <- as.data.frame(colData(object)[, intgroup, drop = FALSE])
+  intgroup.df <- as.data.frame(DESeq2::colData(object)[, intgroup, drop = FALSE])
   group <- if (length(intgroup) > 1) {
     factor(apply(intgroup.df, 1, paste, collapse = " : "))
   }
   else {
-    colData(object)[[intgroup]]
+    DESeq2::colData(object)[[intgroup]]
   }
   d <- data.frame(PC1 = pca$x[, 1],
                   PC2 = pca$x[, 2],
